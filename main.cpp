546d106f0ec3617022d47dcba3498268f288982c
AndyJ202 <andy.joy@pcc.edu>
2026-06-13 13:18:50 +0000

    Complete

--- main.cpp
+++ main.cpp
@@ -1,80 +1,106 @@
-// ------------- FILE HEADER -------------
-// Author ✅: 
-// Assignment ✅:
-// Date ✅:
-// Citations: 
-
-
-// ------------- ZYBOOKS SCORES -------------
-// Chapter ✅: 
-// Participation ✅: 
-// Challenge ✅:
-// Labs ✅:
-
-
-// ------------- DISCORD POSTS -------------
-// https://discord.com/invite/URYKKf8YHm
-// Count ✅:
-// Links (Optional): 
-
-
-// ------------- DESIGN DOCUMENT -------------
-// A. INPUT ✅: 
-// B. OUTPUT ✅:
-// C. CALCULATIONS ✅:
-// D. LOGIC and ALGORITHMS ✅:
-//    (Optional) flow chart link or file name: 
-
-
-// ------------- TESTING -------------
-// PASS ALL GIVEN SAMPLE RUN TESTS ✅: 
-// (Optional) Additional tests count:   
-
-
-// ------------- CODE -------------
+/******************************************************************************
+# Author:           Andy Joy
+# Assignment:       Discussion 8
+# Date:             06/13/2026
+# Description:      This program takes the user's monthy income and expenses.
+#                   Then calculates the total income, total expenses, net total,
+#                   and notifies the user if they're over or under the required
+#                   amount.
+# Input:            socialSecurity (double), pensions (double), retirementSavings
+#                   (double), food (double), housing (double),
+#                   entertainment (double).
+# Output:           totalIncome (double), totalExpenses (double), net total
+#                   (calculated).
+# Sources:          N/A
+#******************************************************************************/
 #include <iostream>
+#include <iomanip>
+#include <cmath>
+#include <limits>
 
 using namespace std;
 
-// Function prototypes (if any)
+//Function prototypes
+void welcomeMessage();
+void goodByeMessage();
+void doubleInputValidation(double &userInteger, string prompt);
+void monthlyIncomeSources(double &income);
+void monthlyExpenses(double &expenses);
+
+//Main function
+int main() {
+  double totalIncome = 0;
+  double totalExpenses = 0;
+
+  welcomeMessage();
+  cout << "\n";
+  monthlyIncomeSources(totalIncome);
+  cout << "\n";
+  monthlyExpenses(totalExpenses);
+  cout << "\n" << "Total Income Amount: $" << totalIncome << endl;
+  cout << "Total Expenses: $" << totalExpenses << endl;
+  cout << "Net total: $" << totalIncome - totalExpenses <<endl;
+
+  if (totalIncome == totalExpenses) {
+    cout << "\nIncome matched the required expenses!" << endl;
+  }
+
+  else if (totalIncome > totalExpenses) {
+    cout << "\nIncome exceeded the required expenses by $" << totalIncome - totalExpenses << "!" << endl;
+  }
+
+  else {
+    cout << "\nOh no! Expenses exceeded the required income by $" << totalExpenses - totalIncome << "." << endl;
+  }
 
 
-// Main function
-// https://en.cppreference.com/w/cpp/language/main_function.html
-int main(int argc, char* argv[]) {
-  cout << "Hello, World!" << endl;
+  goodByeMessage();
+
   return 0;
 }
 
-// Function implementations (if any)
+void welcomeMessage() {
+  cout << "Welcome to my budgeting program." << endl;
+}
 
+void goodByeMessage() {
+  cout << "\nThank you for using my program." << endl;
+}
 
-// ------------- DESIGN -------------
-/* 
-Program Name:
+void doubleInputValidation(double &userDouble, string prompt) {
+    cout << prompt;
+    cin >> userDouble;
+  
+  while (!cin || userDouble < 0) {
+    cout << "Invalid input, please try again!" << endl;
+    cin.clear();
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+    cout << prompt;
+    cin >> userDouble;
+  }
+  cin.ignore(numeric_limits<streamsize>::max(), '\n');
+}
 
-Program Description:
+void monthlyIncomeSources(double &income) {
+  double socialSecurity = 0.0;
+  double pensions =  0.0;
+  double retirementSavings = 0.0;
+  
+  doubleInputValidation(socialSecurity, "Please enter the monthly income amount from social security: ");
+  doubleInputValidation(pensions, "Please enter the monthly income amount from pensions: ");
+  doubleInputValidation(retirementSavings,"Please enter the monthly income amount from retirement savings: ");
 
-Design:
-A. INPUT
-Define the input variables including name data type. 
+  income = socialSecurity + pensions + retirementSavings;
+}
 
-B. OUTPUT
-Define the output variables including data types. 
+void monthlyExpenses(double &expenses) {
+  double food = 0.0;
+  double housing =  0.0;
+  double entertainment = 0.0;
+  
+  doubleInputValidation(food, "Please enter the monthly expenses for food: ");
+  doubleInputValidation(housing, "Please enter the monthly expenses for housing: ");
+  doubleInputValidation(entertainment,"Please enter the monthly expenses for entertainment: ");
 
-C. CALCULATIONS
-Describe calculations used by algorithms in step D.  
-List all formulas. 
-If there are no calculations needed, state there are no calculations.
-
-D. LOGIC and ALGORITHMS
-Design the logic of your program using pseudocode or flowcharts. 
-Use conditionals, loops, functions or array constructs.
-List the steps in transforming inputs into outputs. 
-https://github.com/Glen-Sasek-PCC-Instructor/2025-06-22/blob/main/Pseudocode-Reference.txt
-
-
-SAMPLE RUNS
-Copy from assignment document.
-
-*/
+  expenses = food + housing + entertainment;
+}
